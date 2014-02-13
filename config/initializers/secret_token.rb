4aa52090863464c5b8cb772ec662bf67de4093ac
blackwidowmaker <jtptillman@gmail.com>
2014-02-13 16:43:16 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TasteMarkets::Application.config.secret_key_base = 'd355eb5bfe8a54e4c2fedad7acf18331a48b23776954d44b353012a9be2bc5e86811df4cc90b5b58b0d425c3d413085a7d4acd054275ba7966e7c086d66c9f2d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TasteMarkets::Application.config.session_store :cookie_store, key: '_TasteMarkets_session'
