9774ca6af1164877247e0b30b4601c44ddc91768
blackwidowmaker <jtptillman@gmail.com>
2014-02-14 03:03:53 -0600

    created home and about pages with layout and views

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def home
+  	#database interaction here
+  end
+
+  def about
+  	#database interaction here
+  end
+  def login
+  	#database interaction here
+  end
+end
